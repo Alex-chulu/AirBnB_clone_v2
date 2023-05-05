19b3783bf90ccc0d9e8b47c7e607ec74515a9a60
Alex-Chulu <chulualexandar@gmail.com>
2023-05-05 01:45:58 +0000

    task 2 fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,42 +1,41 @@
 #!/usr/bin/env bash
 
-from datetime import datetime
-from fabric.api import env, put, run
-import os
+from fabric.api import env, put, run, sudo
+import os.path
 
-# Update the env.hosts list with the IP addresses of the web servers
-env.hosts = ['<IP web-01>', '<IP web-02>']
-env.user = '<username>'
-env.key_filename = '<path to SSH private key>'
+env.hosts = ['35.153.78.21', '54.172.82.247']
+env.user = 'ubuntu'
+env.key_filename = 'my_ssh_private_key'
 
-def do_pack():
-    """Creates a compressed archive of the web_static folder"""
-    try:
-        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-        archive_path = 'versions/web_static_{}.tgz'.format(timestamp)
-        local('mkdir -p versions')
-        local('tar -czvf {} web_static'.format(archive_path))
-        return archive_path
-    except:
-        return None
-
-def do_deploy(archive_path):
-    """Distributes an archive to your web servers"""
-    if not os.path.exists(archive_path):
+def do_deploy(versions/web_static.tgz):
+    """Distribute an archive to web servers."""
+    if not os.path.exists(versions/web_static.tgz):
         return False
 
     try:
         # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, '/tmp/')
+        put(versions/web_static.tgz, "/tmp/")
 
-        # Uncompress the archive to the folder /data/web_static/releases/<archive filename without extension>
-        filename = os.path.basename(archive_path)
-        foldername = '/data/web_static/releases/' + filename[:-4]
-        run('mkdir -p {}'.format(foldername))
-        run('tar -xzf /tmp/{} -C {}'.format(filename, foldername))
+        # Uncompress the archive to the folder /data/web_static/releases/<archive 
+	# filename without extension> on the web server
+        filename = os.path.basename(versions/web_static.tgz)
+        directory = "/data/web_static/releases/" + filename.split(".")[0]
+        sudo("mkdir -p {}".format(directory))
+        sudo("tar -xzf /tmp/{} -C {} --strip-components 1".format(filename, directory))
 
         # Delete the archive from the web server
-        run('rm /tmp/{}'.format(filename))
+        sudo("rm /tmp/{}".format(filename))
 
-        # Move files from /data/web_static/releases/<archive filename without extension>/web_static/ to /data/web_static/releases/<archive filename without extension>/
-        run('mv {}/web_static/* {}'.format
+        # Delete the symbolic link /data/web_static/current from the web server
+        sudo("rm -f /data/web_static/current")
+
+        # Create a new symbolic link /data/web_static/current on the web server, 
+	# linked to the new version of your code
+        sudo("ln -s {} /data/web_static/current".format(directory))
+
+        # Return True if successful
+        return True
+    except:
+        # Return False if unsuccessful
+        return False
+
